7beab3cef935bf318ec75b4ef2c1f9c9adbd9e20
PiyushTongya <piyushtongya21@gmail.com>
2023-07-24 12:27:41 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="app">
+    <header>
+      <div class="order">
+      
+    <button @click="handleClick('title')">order by title</button>
+    <button @click="handleClick('salary')">order by salary</button>
+    <button @click="handleClick('location')">order by location</button>
+   
+      </div>
+    </header>
+    <jobsList :jobs="jobs" :order="order"/>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue';
+import JobsList from './components/JobsList.vue';
+import Job from './components/types/job';
+import OrderTerm from './components/types/OrderTerm';
+
+
+export default defineComponent({
+  name: 'App',
+  components: {JobsList},
+  setup() {
+    const jobs = ref<Job[]>([
+      { title: "farm worker", location: "Jaipur", salary: 2345, id: "1" },
+      { title: "worker", location: "Rajasthan", salary: 20000, id: "2" },
+      { title: "fam", location: "Chittorgarh", salary: 20345, id: "3" },
+      { title: "horn", location: "Bhilwara", salary: 4555, id: "4" },
+      { title: "Hosted", location: "Delhi", salary: 3444, id: "5" },
+      { title: "Vue", location: "landon", salary: 45000, id: "6" },
+      { title: "react", location: "hydrabad", salary: 6444, id: "7" }
+    ]);
+      const order = ref<OrderTerm>('title')
+    const handleClick=(term:OrderTerm)=>{
+      order.value = term
+    }
+
+    return {
+      jobs,handleClick,order
+    };
+  }
+});
+</script>
+
+<style>
+header{
+  text-align: center;
+}
+header .order{
+  margin-top: 20px;
+}
+button{
+  margin:  0 20px;
+  color: #1195c9;
+   border:3px solid #1195c9;
+   background: #d5f0ff;
+   padding: 8px 16px;
+   cursor: pointer;
+   font-weight: bold;
+}
+</style>
---
+++ src/components/JobsList.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="job-list">
+    <p>Order by {{ order }}</p>
+
+    <transition-group name="list" tag="ul">
+      <li v-for="job in orderedJobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <img src="../assets/money.png" alt="ruppess" />
+          <p>{{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry's standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum.
+          </p>
+        </div>
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from "vue";
+import Job from "./types/job";
+import OrderTerm from "./types/OrderTerm";
+
+export default defineComponent({
+  props: {
+    jobs: {
+      required: true,
+      type: Array as PropType<Job[]>,
+    },
+    order: {
+      required: true,
+      type: String as PropType<OrderTerm>,
+    },
+  },
+  setup(props) {
+    const orderedJobs = computed(() => {
+      return [...props.jobs].sort((a: Job, b: Job) => {
+        return a[props.order] > b[props.order] ? 1 : -1;
+      });
+    });
+    return { orderedJobs };
+  },
+});
+</script>
+
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 50px;
+}
+.salary p {
+  color: powderblue;
+  font-weight: bold;
+  margin: 10px 4px;
+  font-size: 25px;
+}
+.list-move {
+  transition: all 1s;
+}
+</style>
